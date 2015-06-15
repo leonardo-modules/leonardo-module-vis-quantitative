fda63d35f055f4c9cc7fa63d9c807320c0b40031
Ales Komarek <mail@newt.cz>
2015-06-15 19:07:40 +0200

    fix admin

--- leonardo_module_vis_quantitative/admin.py
+++ leonardo_module_vis_quantitative/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from django.utils.translation import ugettext_lazy as _
 
-class QuantitativeDataSource(admin.ModelAdmin):
+class QuantitativeDataSourceAdmin(admin.ModelAdmin):
     list_display = ['name', 'type', 'data']
 
 admin.site.register(QuantitativeDataSource, QuantitativeDataSourceAdmin)
