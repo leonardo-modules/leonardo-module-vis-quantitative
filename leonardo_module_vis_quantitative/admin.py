e2d54f0a3f6b5d88dd5e46bef79377e3c82cd56b
Ales Komarek <mail@newt.cz>
2015-06-15 19:09:56 +0200

    import

--- leonardo_module_vis_quantitative/admin.py
+++ leonardo_module_vis_quantitative/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 from django.utils.translation import ugettext_lazy as _
 
+from models import QuantitativeDataSource
+
 class QuantitativeDataSourceAdmin(admin.ModelAdmin):
     list_display = ['name', 'type', 'data']
 
