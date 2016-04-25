5e27538a32689d3af2ccf098af7b6b2dc1c85a10
Michael Kutý <6du1ro.n@gmail.com>
2016-04-25 21:58:51 +0200

    Fix all type of data.

--- leonardo_module_vis_quantitative/models.py
+++ leonardo_module_vis_quantitative/models.py
@@ -408,12 +408,20 @@ class NumericWidget(TemporalDataWidget):
                         'value': value
                     })
                 else:
-                    data.append({
-                        'value': reduce(
-                            lambda x, y: x[0] + y[0],
-                            json_not_none) / len(json_not_none),
-                        'label': metric['name']
-                    })
+                    try:
+                        data.append({
+                            'value': reduce(
+                                lambda x, y: x[0] + y[0],
+                                json_not_none)[0] / len(json_not_none),
+                            'label': metric['name']
+                        })
+                    except:
+                        data.append({
+                            'value': reduce(
+                                lambda x, y: x[0] + y[0],
+                                json_not_none) / len(json_not_none),
+                            'label': metric['name']
+                        })
 
         return data
 
@@ -424,8 +432,8 @@ class NumericWidget(TemporalDataWidget):
         data = []
         for metric in self.get_metrics():
             data.append({
-                'label': metric['name'],
-                'value': randint(100, 200)
+#                'key': metric['name'],
+                'value': randint(0, 100)
             })
         return data
 
@@ -433,10 +441,24 @@ class NumericWidget(TemporalDataWidget):
         data = []
         for metric in self.get_metrics():
             data.append({
-                'label': metric['name'],
-                'value': randint(100, 200)
+#                'key': metric['name'],
+                'value': randint(0, 100)
             })
         return data
 
     class Meta:
         abstract = True
+
+#    tabs = {
+#        'Format': {
+#            'name': _('Format'),
+#            'fields': ('duration_length', 'duration_unit',
+#                       'low_horizon', 'high_horizon')
+#        },
+#        'Data': {
+#            'name': _('Data'),
+#            'fields': ('step_length', 'step_length',
+#                       'step_unit', 'step_fun',
+#                       'start', 'align_to_from')
+#        }
+#    }
