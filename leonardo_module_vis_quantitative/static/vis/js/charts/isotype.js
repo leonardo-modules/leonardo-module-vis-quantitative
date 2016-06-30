b9f7a0cd95ee50286479c18fe875bbfdd7a5a1c3
Adam Tengler <a.tengler@tcpcloud.eu>
2016-06-30 20:02:29 +0200

    Test custom SVG icon for isotype chart

--- leonardo_module_vis_quantitative/static/vis/js/charts/isotype.js
+++ leonardo_module_vis_quantitative/static/vis/js/charts/isotype.js
@@ -47,12 +47,32 @@ var leonardo = function(leonardo) {
                 .attr("preserveAspectRatio", "xMinYMin meet");
 
             //define an icon store it in svg <defs> elements as a reusable component - this geometry can be generated from Inkscape, Illustrator or similar
-            chart.append("defs")
-                .append("g")
-                .attr("id","iconCustom")
-                .append("path")
-                        .attr("d","M3.5,2H2.7C3,1.8,3.3,1.5,3.3,1.1c0-0.6-0.4-1-1-1c-0.6,0-1,0.4-1,1c0,0.4,0.2,0.7,0.6,0.9H1.1C0.7,2,0.4,2.3,0.4,2.6v1.9c0,0.3,0.3,0.6,0.6,0.6h0.2c0,0,0,0.1,0,0.1v1.9c0,0.3,0.2,0.6,0.3,0.6h1.3c0.2,0,0.3-0.3,0.3-0.6V5.3c0,0,0-0.1,0-0.1h0.2c0.3,0,0.6-0.3,0.6-0.6V2.6C4.1,2.3,3.8,2,3.5,2z");
 
+            if(config.hasOwnProperty("iconUrl")){
+                chart.append("defs")
+                    .append("g")
+                        .attr("id","iconCustom")
+                    .append("image")
+                        .attr("xlink:href",config.iconUrl)
+                        .attr("width","8")
+                        .attr("height","8")
+            }else{
+                chart.append("defs")
+                    .append("g")
+                        .attr("id","iconCustom")
+                    .append("path")
+                        .attr("d","M3.5,2H2.7C3,1.8,3.3,1.5,3.3,1.1c0-0.6-0.4-1-1-1c-0.6,0-1,0.4-1,1c0,0.4,0.2,0.7,0.6,0.9H1.1C0.7,2,0.4,2.3,0.4,2.6v1.9c0,0.3,0.3,0.6,0.6,0.6h0.2c0,0,0,0.1,0,0.1v1.9c0,0.3,0.2,0.6,0.3,0.6h1.3c0.2,0,0.3-0.3,0.3-0.6V5.3c0,0,0-0.1,0-0.1h0.2c0.3,0,0.6-0.3,0.6-0.6V2.6C4.1,2.3,3.8,2,3.5,2z");
+            }
+
+            //define color filter
+
+            chart.selectAll("defs")
+                .append("filter")
+                    .attr("id","colorMask")
+                .append("feColorMatrix")
+                    .attr("type","matrix")
+                    .attr("values","1 0 0.5 0.5 0.5, 0 1 0 0 0, 0 0 1 0 0, 0 0 0 1 0")
+                
             //specify the number of columns and rows for pictogram layout
             var numCols = 10;
             var numRows = 10;
@@ -94,6 +114,10 @@ var leonardo = function(leonardo) {
             self.instances[config.chartSelector].chart = chart;
             self.render(config.chartSelector);
 
+            //apply filter to selected icons
+
+            chart.selectAll("use.iconSelected")
+                .attr("filter","url(#colorMask)")
         };
     };
     leonardo.charts.initChart("isotype", new Isotype());
